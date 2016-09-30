b1b36a6ab5d1ebf3c0be5bc98539412d4d371b9c
Yubo Allan Diwu <yubo.diwu@gmail.com>
2016-09-29 19:51:30 -0700

    pixel art maker

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const CANVAS_PIXEL_CLASS = 'canvas-pixel';
 
 // TODO: globl brush color is not ideal, figure out how to fix this...
 let brushColor = 'red';
+let body = document.getElementsByTagName('body');
 
 // MAIN ENTRY
 window.onload = function main() {
-  buildPallet(undefined, 20);
-  buildCanvas(80, 80, 7);
+    buildPallet(undefined, 40);
+    buildCanvas(70, 140, 10);
 }
 
 /**
@@ -18,27 +19,27 @@ window.onload = function main() {
  * is a div, and has an event listener which sets the brush color on click.
  */
 function buildPallet(colors, pxSize = 10) {
-  let pallet = document.getElementById('pallet');
+    let pallet = document.getElementById('pallet');
 
-  if(colors === undefined) {
-    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-  }
+    if (colors === undefined) {
+        colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'white'];
+    }
 
-  for(let i = 0; i < colors.length; i++) {
-    let curDiv = document.createElement('div');
+    for (let i = 0; i < colors.length; i++) {
+        let curDiv = document.createElement('div');
 
-    curDiv.className = 'pallet-pixel';
-    curDiv.style.height = pxSize + 'px';
-    curDiv.style.width = pxSize + 'px';
-    curDiv.style.backgroundColor = colors[i];
+        curDiv.className = 'pallet-pixel';
+        curDiv.style.height = pxSize + 'px';
+        curDiv.style.width = pxSize + 'px';
+        curDiv.style.backgroundColor = colors[i];
 
-    pallet.appendChild(curDiv);
-  }
+        pallet.appendChild(curDiv);
+    }
 
-  let twoThirdsWidth = Math.floor(2 * window.innerWidth / 3);
-  pallet.style.width = Math.min(twoThirdsWidth, colors.length * (pxSize+2)) + 'px';
+    let twoThirdsWidth = Math.floor(2 * window.innerWidth / 3);
+    pallet.style.width = Math.min(twoThirdsWidth, colors.length * (pxSize + 12) + colors.length * 20) + 'px';
 
-  pallet.addEventListener('click', setBrushColorHandler);
+    pallet.addEventListener('click', setBrushColorHandler);
 }
 
 /**
@@ -47,50 +48,82 @@ function buildPallet(colors, pxSize = 10) {
  * an event listener attached to set their color to the current brushColor.
  */
 function buildCanvas(height = 100, width = 100, pxSize = 10) {
-  let canvas = document.getElementById('canvas');
+    let canvas = document.getElementById('canvas');
 
-  for(let i = 0; i < width; i++) {
-    for(let j = 0; j < height; j++) {
-      let curDiv = document.createElement('div');
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            let curDiv = document.createElement('div');
 
-      curDiv.className = 'canvas-pixel';
-      curDiv.style.height = pxSize + 'px';
-      curDiv.style.width = pxSize + 'px';
+            curDiv.className = 'canvas-pixel';
+            curDiv.style.height = pxSize + 'px';
+            curDiv.style.width = pxSize + 'px';
 
-      canvas.appendChild(curDiv);
+            canvas.appendChild(curDiv);
+        }
     }
-  }
 
-  // Fit the canvas to the pixel size -- not "responsively" because pixel art isn't "responsive".
-  canvas.style.width = ((width) * (pxSize+2)) + 'px';
-  canvas.style.height = ((height) * (pxSize+2)) + 'px';
-  canvas.addEventListener('click', setBackgroundColorHandler);
+    // Fit the canvas to the pixel size -- not "responsively" because pixel art isn't "responsive".
+    canvas.style.width = ((width) * (pxSize + 2)) + 'px';
+    canvas.style.height = ((height) * (pxSize + 2)) + 'px';
+    console.log(body)
+
+    window.addEventListener('mousedown', clickAndDrag);
+
+    window.addEventListener('mouseup', checkEventListener);
+
+    window.addEventListener('mouseup', stopPainting);
+
 }
 
+function checkEventListener(event) {
+    console.log(event.target);
+    console.log('mouseup triggered');
+}
+
+function stopPainting(event) {
+    canvas.removeEventListener('mouseover', setBackgroundColorHandler)
+}
+
+// TRYING TO MAKE A REAL PAINTBRUSH
+function clickAndDrag(event) {
+    console.log('mousedown triggered')
+    canvas.addEventListener('mouseover', setBackgroundColorHandler);
+    // canvas.addEventListener('mouseup',)
+    // canvas.removeEventListener('mouseup',setBackgroundColorHandler);
+    // canvas.removeEventListener('mouseup',setBackgroundColorHandler)
+}
+
+
+// function addColor2Px(event) {
+//     event.target
+// }
 /**
  * An event handler to set the background color of the target element to the current
  * brushColor.
  */
 function setBackgroundColorHandler(event) {
-  // Never color the whole canvas
-  if(event.target === event.currentTarget) {
-    return;
-  }
+    // Never color the whole canvas
+    console.log(event.target.id);
 
-  event.target.style.borderColor = brushColor;
-  event.target.style.backgroundColor = brushColor;
+
+    event.target.style.borderColor = brushColor;
+    event.target.style.backgroundColor = brushColor;
+    // return;
+
+
 }
-
 
 /**
  * An event handler to set the current brushColor to the background color of the
  * event which was clicked.
  */
 function setBrushColorHandler(event) {
-  // Never set the brush color to the pallets bg color
-  if(event.target === event.currentTarget) {
-    return;
-  }
+    // Never set the brush color to the pallets bg color
 
-  brushColor = event.target.style.backgroundColor;
+    if (event.target === event.currentTarget) {
+        return;
+    }
+
+    brushColor = event.target.style.backgroundColor;
+    console.log(brushColor)
 }
